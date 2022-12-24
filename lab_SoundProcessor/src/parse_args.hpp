33e88212b954fc0f6b999a8f737ece8870f9fb94
Андрей Макалев <a.makalev@g.nsu.ru>
2022-12-24 06:46:09 +0000

    Revert "lab3_SoundProcessor (pull request #4)"

--- lab_SoundProcessor/src/parse_args.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Processor.hpp"
-
-void parse_args(std::string &output_file, std::string &config_file, std::vector<std::string> &input_files, int argc, char *argv[]);
-
-int start(int argc, char *argv[]);
